f71605ac191fe934b062f8cfa9226b660a27b129
danco <danco@192.168.25.15>
2018-06-18 09:38:29 -0300

    Adicionar Palavras

--- src/goodle/controller/Controller.java
+++ src/goodle/controller/Controller.java
@@ -8,29 +8,37 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.Scanner;
 
+/**
+ *
+ * @author danco
+ */
 public class Controller {
-    
+
     Ilist listaPalavras = new LinkedList();
 
+    /**
+     * Ler os arquivos de texto e salva dentro de uma lista
+     */
     public void AdicionarPalavras() {
-  
+
         Pagina pagina;
         Palavra palavras;
         String palavra;
-        
+
+        //Só ta lendo 1 arquivo
         try {
             File arq = new File("jogadoresCadastrados.txt");
-            
+
             pagina = new Pagina(arq);
             Scanner scan = new Scanner(new FileReader(arq)).useDelimiter(" |\n");
-            
+
             //Falta a condição caso a palavra já tenha aparecido ateriormente
             while (scan.hasNext()) {
                 palavra = scan.next();
 
                 palavras = new Palavra(palavra);
                 palavras.Pagina(pagina);
-                
+
                 listaPalavras.addLast(palavras);
             }
             scan.close();
@@ -38,9 +46,12 @@ public class Controller {
             System.out.println("Erro ao ler arquivo!");
         }
     }
-    
-    public void Buscar(){
+
+    /**
+     * Faz a busca da palavra que o usuário deseja
+     * @param palavra palavra que o usuário deseja buscar
+     */
+    public void Buscar(Palavra palavra) {
     }
 
-    
 }
