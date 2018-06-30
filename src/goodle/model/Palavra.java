db84a57c9610ce8833524eb25c4a01a6ce271d60
Daniel <38465439+dandamnn@users.noreply.github.com>
2018-06-30 18:45:07 -0300

    Update Palavra.java

--- src/goodle/model/Palavra.java
+++ src/goodle/model/Palavra.java
@@ -5,56 +5,83 @@ import goodle.util.LinkedList;
 
 /**
  *
- * @author danco
+ * @author Daniel Alves e Gabriela Nunes
  */
 public class Palavra implements Comparable {
 
     private String palavra;
     private int quantidade, buscas;
+    private Pagina pagina;
     private Ilist lPagina;
 
+    /**
+     * Construtor da classe
+     *
+     * @param palavra palavra que foi lida da página
+     * @param pagina pagina em que a palavra foi lida pela primeira vez
+     */
+    public Palavra(String palavra, Pagina pagina) {
+        this.palavra = palavra;
+        this.pagina = pagina;
+        this.quantidade = 1;
+        this.buscas = 0;
+        this.lPagina = new LinkedList();
+        setPagina();
+    }
+
+    /**
+     * Método para adicionar a primeira página na lista
+     */
+    public void setPagina() {
+        this.pagina.setQuantDaPalavra();
+        lPagina.addLast(this.pagina);
+    }
+
+    /**
+     *
+     * @return primeira página em que a palavra aparece
+     */
+    public Pagina getPagina() {
+        return this.pagina;
+    }
+
+    /**
+     *
+     * @return palavra lida
+     */
     public String getPalavra() {
         return palavra;
     }
 
-    public void setPalavra(String palavra) {
-        this.palavra = palavra;
-    }
-
+    /**
+     *
+     * @return quantidade total da palavra
+     */
     public int getQuantidade() {
         return quantidade;
     }
 
-    public void setQuantidade(int quantidade) {
-        this.quantidade = quantidade;
-    }
-
+    /**
+     *
+     * @return quantidade de vezes que a palavra foi buscada
+     */
     public int getBuscas() {
         return buscas;
     }
 
-    public void setBuscas(int buscas) {
-        this.buscas = buscas;
-    }
-
+    /**
+     *
+     * @return lista com as páginas em que a palavra aparece
+     */
     public Ilist getlPagina() {
         return lPagina;
     }
 
-    public void setlPagina(Ilist lPagina) {
-        this.lPagina = lPagina;
-    }
-
     /**
-     * Controller da classe
-     *
-     * @param palavra
+     * Contador para atualizar a quantidade de vezes que essa palavra foi lida
      */
-    public Palavra(String palavra) {
-        this.palavra = palavra;
-        this.quantidade = 1;
-        this.buscas = 0;
-        lPagina = new LinkedList();
+    public void quantidade() {
+        this.quantidade = this.quantidade + 1;
     }
 
     /**
@@ -62,29 +89,24 @@ public class Palavra implements Comparable {
      *
      * @param pagina pagina em que a palavra aparece
      */
-    public void Pagina(Pagina pagina) {
-        pagina.Quantidade();
+    public void addPagina(Pagina pagina) {
         this.lPagina.addLast(pagina);
     }
 
     /**
      * Contador para a quantidade de vezes que essa palavra foi buscada
      */
-    public void Busca() {
+    public void buscas() {
         this.buscas = this.buscas + 1;
     }
 
     /**
-     * Atualiza a quantidade de vezes que a palavra aparece
+     * Compara dois objetos do tipo "palavra" de acordo com seu nome
      *
-     * @param quant
-     * @return
+     * @param p
+     * @return "1" se essa palavra for maior do que a dada por parâmetro, "-1"
+     * se for maior e "0" se forem iguais
      */
-    public int Quantidade(int quant) {
-        this.quantidade = quant;
-        return quantidade;
-    }
-
     @Override
     public int compareTo(Object p) {
         String palavra = (String) p;
@@ -96,4 +118,14 @@ public class Palavra implements Comparable {
             return 0;
         }
     }
+
+    /**
+     *
+     * @return o nome da palavra
+     */
+    @Override
+    public String toString() {
+        return palavra;
+    }
 }
+
